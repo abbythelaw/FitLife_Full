b8769cd0c78b24cabe969ab1f47009ff1f502e8a
Abner Serania <140513262+abbythelaw@users.noreply.github.com>
2026-09-07 21:18:32 +0000

    Save current FitLife Snapshot implementation

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,21 @@ import './features/media/media-refinement.css'
 import './styles/default-colorful.css'
 
 import './styles/consolidated-comments.css'
+
+import './features/snapshot/SnapshotV5.css'
+
+import './features/snapshot/SnapshotV6.css'
+
+import './features/snapshot/SnapshotV7.css'
+
+import './features/snapshot/SnapshotV8.css'
+
+import './features/snapshot/SnapshotV9.css'
+
+import './features/snapshot/SnapshotV10.css'
+
+import './features/snapshot/SnapshotV11Updated.css'
+
+import './features/snapshot/SnapshotV12Labels.css'
+
+import './features/snapshot/SnapshotV14Alignment.css'
